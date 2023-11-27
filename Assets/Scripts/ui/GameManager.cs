b9f826915126626033ab49e46a0548354ba11f9c
SoraxDubbing <sorax5.r23@gmail.com>
2023-11-27 11:16:14 +0100

    Add logo

---
+++ Assets/Scripts/ui/GameManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using logic;
+using logic.models;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+using UnityEngine.Tilemaps;
+
+/// <summary>
+/// Main class of the game.
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    
+    #region Singleton
+    
+    private static GameManager instance;
+    
+    private GameManager()
+    {
+        
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    #endregion
+
+    [SerializeField] private ScriptableWorldSettings settings;
+    [SerializeField] private List<TileBase> tiles;
+    [SerializeField] private List<BlockType> types;
+
+    private TileBlockTypeFabric fabric;
+
+    public ScriptableWorldSettings Settings => this.settings;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*fabric = new TileBlockTypeFabric();
+
+        foreach (var tileBase in tiles)
+        {
+            foreach (var blockType in types)
+            {
+                BlockTypeConstructor constructor = new BlockTypeConstructor(tileBase);
+                fabric.Register(blockType,constructor);
+            }
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public static GameManager Instance => instance;
+}
